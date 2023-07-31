f6b581a2af8d84e70ead508ea661c307405117c5
Oleksandra-lab <tka4olex@gmail.com>
2023-07-31 23:50:24 +0300

    toDo list almost done

--- scrpt02.js
+++ scrpt02.js
@@ -1,36 +1,56 @@
-const form = document.querySelector('.js-form')
-form.addEventListener('submit', hendleForm)
-const arr = JSON.parse(localStorage.getItem("card")) ?? [] ;
+const form = document.querySelector('.js-form');
+const listContainer = document.querySelector('.js-list');
+const arr = JSON.parse(localStorage.getItem("card")) ?? [];
+form.addEventListener('submit', hendleForm);
+
+
 function hendleForm(evt){
     evt.preventDefault()
-
-    const{title, text} = evt.currentTarget.elements
-    const markup = `<li>
-    <h3>${title.value}</h3>
-    <p>${text.value}</p>
+    const title = evt.target.elements.title.value;
+    const description = evt.target.elements.descr.value;
+    const id = Math.random().toString(36).substring(2);
+    const object = {
+        title,
+        description,
+        id
+    }
+    const markup = `<li data-id =${id}>
+    <h3>${title}</h3>
+    <p>${description}</p>
 </li>`
 listContainer.insertAdjacentHTML("beforeend", markup)
 
-const object = {
-    title,
-    decription
-}
 
-arr.push(object)
+
+arr.shift(object)
 localStorage.setItem("card", JSON.stringify(arr))
-    console.dir(title.value);
-    console.dir(text.value);
 
-    
-
+evt.target.elements.title.value = " ";
+evt.target.elements.descr.value = " ";
 }
-const listContainer = document.querySelector('.js-card')
+
 
 function markup(arr){
-    return arr.map({title, description}) => `<li>
-    <h3>${title.value}</h3>
-    <p>${text.value}</p>
-</li>`.join("")
+    return arr.map(({title, description, id, completed}) => `<li class="${completed?"complete":""}" data-id =${id}> 
+    <h3>${title}</h3>
+    <p>${description}</p>
+</li>`).join("")
     
 }
-listContainer.insertAdjacentHTML("afterbegin", markup(arr))
+listContainer.insertAdjacentHTML("afterbegin", markup(arr));
+
+listContainer.addEventListener("click", onClick)
+
+function onClick(e){
+    if(e.target === e.currentTarget) return;
+    
+    const item = e.target.closest("li");
+    
+    item.classList.add("complete");
+
+    const findElement = arr.find(({id}) => id === item.dataset.id)
+    findElement.completed = true
+
+    localStorage.setItem("card", JSON.stringify(arr))
+}
+
